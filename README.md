1b3a19633039ad6c212913e4a9c53e132fdca992
Peter <peterdalle@users.noreply.github.com>
2020-01-05 12:25:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Svensk politik
+
+Hjälpfunktioner för R för att hantera svenska politiska partier.
+
+## Exempel
+
+Plockar ut namn, engelskt namn samt partifärg för Socialdemokraterna (`S`).
+
+```r
+party_names["S"]
+party_names_eng["S"]
+party_colors["S"]
+```
+
+## Källor 
+
+- Partisymboler för svenska partier nedladdade från respektive partis Wikipedia-sida.
---
+++ parties.r
@@ -0,0 +1,45 @@
+party_names <- c(V  = "Vänsterpartiet",
+                 S  = "Socialdemokraterna",
+                 MP = "Miljöpartiet",
+                 C  = "Centern",
+                 FP = "Folkpartiet",
+                 L  = "Liberalerna",
+                 M  = "Moderaterna",
+                 KD = "Kristdemokraterna",
+                 SD = "Sverigedemokraterna",
+                 FI = "Feministiskt initiativ",
+                 PP = "Piratpartiet")
+  
+party_names_eng <- c(V  = "Left Party",
+                     S  = "Social Democrats",
+                     MP = "Green Party",
+                     C  = "Centre Party",
+                     FP = "People's Party",
+                     L  = "Liberals",
+                     M  = "Moderate Party",
+                     KD = "Christian Democrats",
+                     SD = "Sweden Democrats",
+                     FI = "Feminist Initiative",
+                     PP = "Pirate Party")
+  
+party_colors <- c(V  = "#DA291C",
+                  S  = "#E8112d",
+                  MP = "#83CF39",
+                  C  = "#009933",
+                  FP = "#006AB3",
+                  L  = "#006AB3",
+                  M  = "#52BDEC",
+                  KD = "#000077",
+                  SD = "#b6b602", # Mörkare än originalfärgen (#DDDD00), syns bättre på vit bakgrund
+                  FI = "#CD1B68",
+                  PP = "#572B85")
+  
+party_symbols <- c(
+    V  = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/v.png'  width='20' />",
+    S  = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/s.png'  width='20' />",
+    MP = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/mp.png' width='20' />",
+    C  = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/c.png'  width='20' />",
+    L  = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/l.png'  width='20' />",
+    M  = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/m.png'  width='20' />",
+    KD = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/kd.png' width='20' />",
+    SD = "<img src='https://raw.githubusercontent.com/peterdalle/svensk-politik/master/partisymboler/sd.png' width='20' />")
